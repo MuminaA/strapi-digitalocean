8400b4e13241b160e4b74b659ff57910dde1a0c5
mina <muminaabdi13@gmail.com>
2023-09-18 22:16:03 -0400

    first commit

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,19 +1,27 @@
+// module.exports = ({ env }) => ({
+//   proxy: true,
+//   host: "0.0.0.0",
+//   port: process.env.PORT,
+//   url: env("MY_HEROKU_URL"),
+//   app: {
+//     keys: env.array("APP_KEYS"),
+//   },
+//   admin: {
+//     auth: {
+//       secret: env("ADMIN_JWT_SECRET"),
+//     },
+//   },
+//   // cors: {
+//   //   enabled: true,
+//   //   // origin: ['http://localhost:3000', "https://glittery-dango-ab5a6d.netlify.app"],
+//   //   origin:['*'],
+//   // },
+// });
+
 module.exports = ({ env }) => ({
   proxy: true,
-  host: "0.0.0.0",
-  port: process.env.PORT,
-  url: env("MY_HEROKU_URL"),
+  url: env("APP_URL"), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
-  },
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET"),
-    },
-  },
-  cors: {
-    enabled: true,
-    // origin: ['http://localhost:3000', "https://glittery-dango-ab5a6d.netlify.app"],
-    origin:['*'],
   },
 });
