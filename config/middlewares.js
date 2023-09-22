77abee2de9719539e4a5093bb5272bee11cc9c74
mina <muminaabdi13@gmail.com>
2023-09-22 13:36:20 -0400

    minor adjustments

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,13 +2,6 @@ module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
-  // {
-  //   name: 'strapi::cors',
-  //   config: {
-  //     // origin: ['http://localhost:3000', 'https://glittery-dango-ab5a6d.netlify.app']
-  //     origin:['https://glittery-dango-ab5a6d.netlify.app']
-  //   }
-  // },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
